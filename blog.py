32dbdc0fb394c83af952ce9990738887c00a43ee
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-10-08 09:40:14 -0400

    refactor(newpost): newpost.html --> post-form.html

--- blog.py
+++ blog.py
@@ -257,7 +257,7 @@ class PostCreate(PostHandler):
         super(PostCreate, self).get(action, post_id)
         if self.request.path.find('edit') > -1:
             return
-        self.render("newpost.html", action=action)
+        self.render("post-form.html", action=action)
 
     def post(self, post_id):
         """Create new blog post or edit existing one"""
@@ -282,7 +282,7 @@ class PostCreate(PostHandler):
             self.redirect('/post/view/%s' % str(p.key().id()))
         else:
             error = "subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error)
+            self.render("post-form.html", subject=subject, content=content, error=error)
 
 class PostEdit(PostCreate):
     def get(self, action, post_id):
@@ -293,7 +293,7 @@ class PostEdit(PostCreate):
         params['subject'] = self.blog_post.subject
         params['content'] = self.blog_post.content
         params['action'] = action
-        self.render('newpost.html', **params)
+        self.render('post-form.html', **params)
 
 class PostLike(PostHandler):
     def get(self, action, post_id):
